0345ef9134fd3d9fafbee848662a30ac112163b7
HYPER <meerhadikhan364@gmail.com>
2022-10-17 12:59:28 +0500

    test

---
+++ lib/Screen/detailPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}
